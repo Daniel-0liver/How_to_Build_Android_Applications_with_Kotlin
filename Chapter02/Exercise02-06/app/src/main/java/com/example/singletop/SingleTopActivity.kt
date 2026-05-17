52cbd4f0957c1f76017634d56330c012d79b6ce9
Daniel-0liver <dtoliverbr@gmail.com>
2026-05-17 04:52:31 +0100

    feat: add initial project structure with MainActivity, SingleTopActivity, and StandardActivity

---
+++ Chapter02/Exercise02-06/app/src/main/java/com/example/singletop/SingleTopActivity.kt
@@ -0,0 +1,113 @@
+package com.example.singletop
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.singletop.ui.theme.SingleTopTheme
+
+class SingleTopActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SingleTopTheme {
+                SingleTopScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun SingleTopScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier.fillMaxSize()
+    ) { innerPadding ->
+        val context = LocalContext.current
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Text(
+                text = stringResource(R.string.single_top_title),
+                fontSize = 28.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+
+            Button(
+                onClick = {
+                    context.startActivity(
+                        Intent(
+                            context,
+                            SingleTopActivity::class.java
+                        )
+                    )
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(R.string.single_top_launch_mode),
+                    fontSize = 28.sp
+                )
+            }
+
+            Button(
+                onClick = {
+                    context.startActivity(
+                        Intent(
+                            context,
+                            StandardActivity::class.java
+                        )
+                    )
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(R.string.standard_launch_mode),
+                    fontSize = 28.sp
+                )
+            }
+
+            Button(
+                onClick = {
+                    (context as ComponentActivity).finish()
+                },
+            ) {
+                Text(
+                    text = stringResource(R.string.back_text),
+                    fontSize = 28.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SingleTopScreenPreview() {
+    SingleTopScreen()
+}

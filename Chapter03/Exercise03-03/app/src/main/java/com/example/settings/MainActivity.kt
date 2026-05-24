afb1076bb52152b61188b8394bf67f347fa9737a
Daniel-0liver <dtoliverbr@gmail.com>
2026-05-24 09:33:11 +0100

    feat: add initial project setup with MainActivity, Gradle configuration, and theme resources

---
+++ Chapter03/Exercise03-03/app/src/main/java/com/example/settings/MainActivity.kt
@@ -0,0 +1,187 @@
+package com.example.settings
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.settings.ui.theme.HeaderTextStyle
+import com.example.settings.ui.theme.SettingsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SettingsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SettingsContainer(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SettingsContainer(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        SettingHeader()
+        SettingsImage()
+        SettingsCheckBox()
+        SettingsSwitch()
+    }
+}
+
+@Composable
+fun SettingHeader(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 14.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(
+                R.string.app_name
+            ),
+            style = HeaderTextStyle,
+            modifier = Modifier.padding(end = 10.dp)
+        )
+        Icon(
+            imageVector = Icons.Default.Settings,
+            contentDescription = stringResource(
+                R.string.settings_icon_description
+            ),
+        )
+    }
+}
+
+@Composable
+fun SettingsImage(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .padding(start = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(
+                R.string.settings_profile_image
+            ),
+            fontSize = 18.sp
+        )
+        Image(
+            modifier = Modifier
+                .padding(end = 10.dp)
+                .height(34.dp)
+                .clickable {
+                    // TODO: Handle changing the profile image
+                },
+            painter = painterResource(
+                R.drawable.android_picture
+            ),
+            contentDescription = stringResource(
+                R.string.settings_profile_image
+            )
+        )
+    }
+}
+
+@Composable
+fun SettingsCheckBox(modifier: Modifier = Modifier) {
+    var isChecked by remember { mutableStateOf(false) }
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .padding(start = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(
+                R.string.settings_consent
+            ),
+            fontSize = 18.sp,
+            modifier = Modifier
+                .clickable {
+                    isChecked = !isChecked
+                }
+        )
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it },
+        )
+    }
+}
+
+@Composable
+fun SettingsSwitch(modifier: Modifier = Modifier) {
+    var isChecked by remember { mutableStateOf(false) }
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .padding(start = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(
+                R.string.settings_mobile_data
+            ),
+            fontSize = 18.sp,
+            modifier = Modifier.clickable {
+                isChecked = !isChecked
+            }
+        )
+        Switch(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it }
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun SettingsContainerPreview() {
+    SettingsContainer()
+}
+

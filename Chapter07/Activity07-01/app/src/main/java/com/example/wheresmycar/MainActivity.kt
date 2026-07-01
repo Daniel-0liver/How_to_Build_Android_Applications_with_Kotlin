4fffda5c1c1997780b4b94949ce8f5b44cb907d4
Daniel-0liver <dtoliverbr@gmail.com>
2026-07-01 23:56:13 +0100

    feat: Activity 07-01 implement basic Google Maps integration with CameraPositionState and Scaffold layout.

--- Chapter07/Activity07-01/app/src/main/java/com/example/wheresmycar/MainActivity.kt
+++ Chapter07/Activity07-01/app/src/main/java/com/example/wheresmycar/MainActivity.kt
@@ -9,9 +9,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.wheresmycar.ui.theme.WheresMyCarTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +27,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WheresMyCarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        text = "Hello World!",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WhereIsMyCarScreen()
             }
+        }
+    }
+}
+
+@Composable
+fun WhereIsMyCarScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { innerPadding ->
+        var currentLocation by remember {
+            mutableStateOf(
+                LatLng(
+                    0.0,
+                    0.0
+                )
+            )
+        }
+
+        val cameraPositionState = rememberSaveable(
+             currentLocation,
+            saver = CameraPositionState.Saver
+        ) {
+            CameraPositionState(
+                position = CameraPosition.fromLatLngZoom(
+                    currentLocation,
+                    10f
+                )
+            )
+        }
+        GoogleMap(
+            modifier = Modifier
+                .padding(innerPadding),
+            cameraPositionState = cameraPositionState,
+            onMapClick = {}
+        ) {
+
         }
     }
 }
